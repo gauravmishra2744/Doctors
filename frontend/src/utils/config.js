05df964b6557066c5ed7fa48ce1c60bfddc8caa1
Gaurav Mishra <gauravmishra2744@google.com>
2026-06-02 17:26:29 +0530

    fix cloudinary preset and cloud name

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
 export const URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
-export const cloudinary_upload_preset = process.env.REACT_APP_CLOUDINARY_PRESET || "song_upload"
-export const cloud_name = process.env.REACT_APP_CLOUD_NAME || "dtdfdhwj2"
+export const cloudinary_upload_preset = process.env.REACT_APP_CLOUDINARY_PRESET || "medicare_upload"
+export const cloud_name = process.env.REACT_APP_CLOUD_NAME || "dqsei8pjy"
